a1276c076499913fbceb2a71b6ea083999e98b50
Maria2721 <mashaivvas@gmail.com>
2025-01-23 02:07:36 +0300

    feat: add async in function and export, remove the tracking change for  05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,54 +1,38 @@
-const fs = require('fs');
 const { readdir, readFile, writeFile } = require('fs/promises');
 const path = require('path');
 
 const folderPathRead = path.join(__dirname, 'styles');
 const filePathWrite = path.join(__dirname, 'project-dist', 'bundle.css');
 
-let isProcessing = false;
-
-async function bundleCss() {
+async function bundleCss(sourcePath, targetPath) {
   try {
     // Reading the source directory
-    const files = await readdir(folderPathRead, { withFileTypes: true });
+    const files = await readdir(sourcePath, { withFileTypes: true });
 
     const cssFiles = files.filter(
       (file) => file.isFile() && path.extname(file.name) === '.css',
     );
 
-    // Reading all CSS files
+    // Reading all CSS files and writing their contents to a variable
     let data = '';
     for (const file of cssFiles) {
-      const filePath = path.join(folderPathRead, file.name);
+      const filePath = path.join(sourcePath, file.name);
       const content = await readFile(filePath, 'utf-8');
       data += content + '\n';
     }
 
     // Writing styles to the bundle.css file
-    await writeFile(filePathWrite, data);
-    console.log('CSS files writing into bundle.css successfully');
+    await writeFile(targetPath, data);
   } catch (err) {
-    console.error(`Error: ${err.message}`);
+    console.error('Error while bundling CSS:', err.message);
   }
 }
 
-// Tracking changes in the source directory
-fs.watch(folderPathRead, { recursive: false }, async (eventType, filename) => {
-  if (filename && path.extname(filename) === '.css') {
-    console.log(`Change detected: ${eventType} in file ${filename}`);
+if (require.main === module) {
+  // Default copy operation
+  bundleCss(folderPathRead, filePathWrite).then(() => {
+    console.log('CSS bundling completed successfully');
+  });
+}
 
-    if (isProcessing) {
-      console.log('Processing is already in progress!');
-      return;
-    }
-
-    isProcessing = true;
-    try {
-      await bundleCss();
-    } finally {
-      isProcessing = false;
-    }
-  }
-});
-
-bundleCss();
+module.exports = { bundleCss };
